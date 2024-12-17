d275d597e610c9e2c57b4137371c9320283cc2ae
BugHunter-X <askzenai@gmail.com>
2024-12-17 08:52:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example using Tailwind's @apply directive
+<div class="@apply text-red-500">
+  This text should be red.
+</div>
+```
+
+This might cause unexpected behavior if the `text-red-500` utility class is not defined or if there's a conflict with another class.
